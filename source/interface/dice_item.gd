0c0a1212d4dad6085a25f845e6cbadd6e321b60b
Franco Curotto <francocurotto@gmail.com>
2022-03-30 23:06:20 -0300

    progress in dicelib_scroll

--- source/interface/dice_item.gd
+++ source/interface/dice_item.gd
@@ -1,16 +1,26 @@
-extends PanelContainer
+extends MarginContainer
 
 func update_item(dice):
-    var sidesstr = ""
-    for side in dice.sides:
-        sidesstr += side.crest.name[0] + str(side.mult)
-    $HBox/Index.text = "000".substr(len(str(dice.id))) + str(dice.id) + "."
-    $HBox/Name.text = dice.card.name
-    $HBox/TLACont/TLA.text = dice.card.type[0] +  str(dice.level)
-    if dice.card.is_monster():
-        $HBox/AttackCont/Attack.text = str(dice.card.attack) + "A"
-        $HBox/DefenseCont/Defense.text = str(dice.card.defense) + "D"
-        $HBox/HealthCont/Health.text = str(dice.card.health) + "H"
-    $HBox/SidesCont/Sides.text = sidesstr
-    
-    
+	# create dice strings
+	var indexstr = "000".substr(len(str(dice.id))) + str(dice.id) + "."
+	var tlastr = dice.card.type[0] +  str(dice.level)
+	var attackstr = ""
+	var defensestr = "" 
+	var healthstr = ""
+	if dice.card.is_monster():
+		attackstr = "[right]" + str(dice.card.attack) + "A" + "[/right]"
+		defensestr = "[right]" + str(dice.card.defense) + "A" + "[/right]"
+		healthstr = "[right]" + str(dice.card.health) + "A" + "[/right]"
+	var sidesstr = "[right]"
+	for side in dice.sides:
+		sidesstr += side.crest.name[0] + str(side.mult)
+	sidesstr += "[/right]"
+
+	# assign strings
+	$HBox/Index.text = indexstr
+	$HBox/Name.text = dice.card.name
+	$HBox/TLACont/TLA.bbcode_text = tlastr
+	$HBox/AttackCont/Attack.bbcode_text = attackstr
+	$HBox/DefenseCont/Defense.bbcode_text = defensestr
+	$HBox/HealthCont/Health.bbcode_text = healthstr
+	$HBox/SidesCont/Sides.bbcode_text = sidesstr
--- source/interface/dicelib_scroll.gd
+++ source/interface/dicelib_scroll.gd
@@ -3,10 +3,7 @@ extends ScrollContainer
 export (PackedScene) var DiceItem
 
 func fill(dicelib):
-    for key in dicelib:
-        var diceitem = DiceItem.instance()
-        diceitem.update_item(dicelib[key])
-        var button = Button.new()
-        button.text = "ASDF" + str(key)
-        #$VBox.add_child(button)
-        $VBox.add_child(diceitem)
+	for key in dicelib:
+		var diceitem = DiceItem.instance()
+		diceitem.update_item(dicelib[key])
+		$VBox.add_child(diceitem)
