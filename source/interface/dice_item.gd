f958fa14ce73910037f94fc8b439efbbffe825c5
Franco Curotto <francocurotto@gmail.com>
2022-04-04 12:00:00 -0400

    adjust dice scroll

--- source/interface/dice_item.gd
+++ source/interface/dice_item.gd
@@ -4,56 +4,52 @@ const COMMON_ABILITIES = ["FLY", "ARCHER", "TUNNELING", "NEUTRAL"]
 const ICONWIDTH = "15"
 
 func update_item(dice):
-    $HBox/Index.text = "000".substr(len(str(dice.id))) + str(dice.id) + "."
-    $HBox/Name.text = dice.card.name
-    $HBox/TLACont/TLA.bbcode_text = get_tlastr(dice)
-    $HBox/AttackCont/Attack.bbcode_text = get_attackstr(dice.card)
-    $HBox/DefenseCont/Defense.bbcode_text = get_defensestr(dice.card)
-    $HBox/HealthCont/Health.bbcode_text = get_healthstr(dice.card)
-    $HBox/SidesCont/Sides.bbcode_text = get_sidesstr(dice.sides)
-    
+	$HBox/Index.text = "000".substr(len(str(dice.id))) + str(dice.id) + "."
+	$HBox/Name.text = dice.card.name
+	$HBox/TLACont/TLA.bbcode_text = get_tlastr(dice)
+	$HBox/AttackCont/Attack.bbcode_text = get_attackstr(dice.card)
+	$HBox/DefenseCont/Defense.bbcode_text = get_defensestr(dice.card)
+	$HBox/HealthCont/Health.bbcode_text = get_healthstr(dice.card)
+	$HBox/SidesCont/Sides.bbcode_text = get_sidesstr(dice.sides)
+	
 func get_tlastr(dice):
-    var string = "[img=" + ICONWIDTH + "]"
-    string += "res://art/icons/TYPE_" + dice.card.type + ".png[/img]"
-    string += str(dice.level)
-    if dice.card.ability != null:
-        for ability in dice.card.ability:
-            string += "[img=" + ICONWIDTH + "]" 
-            if ability["NAME"] in COMMON_ABILITIES:
-                string += "res://art/icons/ABILITY_" + ability["NAME"] + ".png[/img]"
-            else:
-                string += "res://art/icons/ABILITY" + ".png[/img]"   
-    return string
+	var string = "[img=" + ICONWIDTH + "]"
+	string += "res://art/icons/TYPE_" + dice.card.type + ".png[/img]"
+	string += str(dice.level)
+	if dice.card.ability:
+		string += "[img=" + ICONWIDTH + "]" 
+		string += "res://art/icons/ABILITY" + ".png[/img]"   
+	return string
 
 func get_attackstr(card):
-    if card.is_item():
-        return ""
-    var string = "[right]" + str(card.attack)
-    string += "[img=" + ICONWIDTH + "]"
-    string  += "res://art/icons/CREST_ATTACK.png[/img][/right]"
-    return string
+	if card.is_item():
+		return ""
+	var string = "[right]" + str(card.attack)
+	string += "[img=" + ICONWIDTH + "]"
+	string  += "res://art/icons/CREST_ATTACK.png[/img][/right]"
+	return string
 
 func get_defensestr(card):
-    if card.is_item():
-        return ""
-    var string = "[right]" + str(card.defense)
-    string += "[img=" + ICONWIDTH + "]"
-    string  += "res://art/icons/CREST_DEFENSE.png[/img][/right]"
-    return string
+	if card.is_item():
+		return ""
+	var string = "[right]" + str(card.defense)
+	string += "[img=" + ICONWIDTH + "]"
+	string  += "res://art/icons/CREST_DEFENSE.png[/img][/right]"
+	return string
 
 func get_healthstr(card):
-    if card.is_item():
-        return ""
-    var string = "[right]" + str(card.health)
-    string += "[img=" + ICONWIDTH + "]"
-    string  += "res://art/icons/HEALTH.png[/img][/right]"
-    return string  
+	if card.is_item():
+		return ""
+	var string = "[right]" + str(card.health)
+	string += "[img=" + ICONWIDTH + "]"
+	string  += "res://art/icons/HEALTH.png[/img][/right]"
+	return string  
 
 func get_sidesstr(sides):
-    var sidesstr = "[right]"
-    for side in sides:
-        sidesstr += "[img=" + ICONWIDTH + "]"
-        sidesstr +=  "res://art/icons/CREST_" + side.crest.NAME + ".png[/img]"
-        sidesstr += str(side.mult)
-    sidesstr += "[/right]"
-    return sidesstr
+	var sidesstr = "[right]"
+	for side in sides:
+		sidesstr += "[img=" + ICONWIDTH + "]"
+		sidesstr +=  "res://art/icons/CREST_" + side.crest.NAME + ".png[/img]"
+		sidesstr += str(side.mult)
+	sidesstr += "[/right]"
+	return sidesstr
--- source/interface/test.gd
+++ source/interface/test.gd
@@ -5,7 +5,7 @@ const Engine = preload("res://engine/engine.gd")
 var engine
 
 func _init():
-    engine = Engine.new()
-    
+	engine = Engine.new()
+	
 func _ready():
-    $DicelibScroll.fill(engine.dicelib)
+	$DicelibScroll.fill(engine.dicelib)
