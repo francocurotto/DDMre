c35edcb39430177b456a31f8c671842a0cc24c40
Franco Curotto <francocurotto@gmail.com>
2022-11-08 18:31:22 -0300

    progress in dicepool window

--- source/tests/test.gd
+++ source/tests/test.gd
@@ -1,14 +1,6 @@
 extends Node
 
-const MODDICT = {"DRAGON"      : {1 : Color(0.0, 0.0, 2.4),
-                                  2 : Color(1.4, 0.0, 0.0)},
-                 "SPELLCASTER" : {1 : Color(0.0, 0.0, 2.3),
-                                  2 : Color(0.8, 0.0, 0.0)},
-                 "UNDEAD"      : {1 : Color(0.0, 0.0, 1.1),
-                                  2 : Color(0.8, 0.0, 0.0)},
-                 "BEAST"       : {1 : Color(0.0, 0.0, 1.2),
-                                  2 : Color(1.4, 0.0, 0.0)},
-                 "WARRIOR"     : {1 : Color(0.0, 0.0, 2.8),
-                                  2 : Color(1.5, 0.0, 0.0)},
-                 "ITEM"        : {1 : Color(0.0, 0.0, 1.1),
-                                  2 : Color(1.0, 0.0, 0.0)}}
+func _input(event):
+    if event is InputEventMouseMotion:
+        print(event.position)
+        #$Label.text = str(event.position)
