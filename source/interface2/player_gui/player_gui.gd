af6dbe3746556d02b14445768a0fe6837762d556
Franco Curotto <francocurotto@gmail.com>
2022-11-09 20:20:48 -0300

    progress card info

--- source/interface2/player_gui/player_gui.gd
+++ source/interface2/player_gui/player_gui.gd
@@ -7,6 +7,7 @@ var CardInfo = preload("res://interface2/player_gui/info_display/card_info/card_
 var engine
 var player
 var opponent
+var cardinfo
 
 # onready variables
 onready var dicepool_window = $DicepoolWindow
@@ -24,15 +25,18 @@ func set_duel(_engine, _player, _opponent):
 
 # signals callbacks
 func on_info_button_pressed(card):
-    var cardinfo = create_cardinfo(card)
-    display_cardinfo(cardinfo)
+    create_cardinfo(card)
+    dicepool_window.visible = false
+
+func on_cardinfo_quit():
+    cardinfo.queue_free()
+    dicepool_window.visible = true
 
 # private functions
 func create_cardinfo(card):
-    var cardinfo = CardInfo.instance()
+    cardinfo = CardInfo.instance()
     add_child_below_node(dicepool_window, cardinfo)
+    cardinfo.connect("cardinfo_quit", self, "on_cardinfo_quit")
     cardinfo.set_card(card)
     return cardinfo
-
-func display_cardinfo(cardinfo):
-    dicepool_window.visible = false
+    
