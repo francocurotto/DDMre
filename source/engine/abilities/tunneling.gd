a4477f1c639a62af70008aa0b9d4c3662fe2027d
Franco Curotto <francocurotto@gmail.com>
2023-01-19 08:25:42 -0300

    implement tunneling

--- source/engine/abilities/tunneling.gd
+++ source/engine/abilities/tunneling.gd
@@ -1,4 +1,11 @@
 extends "base_ability.gd"
 
+# preloads
+const PassBehaviorTunnel = preload("res://engine/dungobj/behaviors/pass_behavior_tunnel.gd")
+
 func _init(ability_dict).(ability_dict):
     pass
+
+# public functions
+func on_summon(monster):
+    monster.pass_behavior = PassBehaviorTunnel.new()
---
+++ source/engine/dungobj/behaviors/pass_behavior_tunnel.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# public functions
+func is_passable(dungobj):
+    """
+    Return true if pass behavior allows to pass dungobj during movement.
+    """
+    return dungobj.is_none() or dungobj.is_monster()
