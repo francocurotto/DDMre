f274b83dd14c0c7d03cda9685897322982716e32
Franco Curotto <francocurotto@gmail.com>
2023-04-03 07:49:24 -0400

    find node test

--- source/tests/test.gd
+++ source/tests/test.gd
@@ -1,4 +1,13 @@
 extends Node
 
-func _on_TextureButton_pressed():
-    print("pressed")
+onready var gc = $"%grandchild"
+#onready var grandchild2 = $"%grandchild/%grandchild2"
+onready var gc2 = find_node("grandchild2")
+
+func _ready():
+    print(gc.name)
+    print(gc2.name)
+    #var _gc2 = get_node("%grandchild/%grandchild2")
+    #var _gc2 = get_node("%grandchild/child2/grandchild2")
+    #var _gc2 = get_node("child/grandchild/%grandchild2")
+
