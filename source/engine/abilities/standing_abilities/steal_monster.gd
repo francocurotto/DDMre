e29f79f8b41529c0a236fa68ef8329afe0fbc507
Franco Curotto <francocurotto@gmail.com>
2023-04-11 09:39:44 -0400

    fix steal monster

--- source/engine/abilities/standing_abilities/steal_monster.gd
+++ source/engine/abilities/standing_abilities/steal_monster.gd
@@ -16,7 +16,9 @@ func activate(activate_dict):
     monster.player.crestpool.remove_crests(crest, cost)
     var opponent_monster = dungeon.get_tile(activate_dict["pos"]).content
     opponent_monster.switch_player()
+    var tile = monster.tile
     monster.die()
+    tile.move_content_from(opponent_monster.tile)
 
 func get_select_tiles():
     return get_opponent_monsters_tiles()
