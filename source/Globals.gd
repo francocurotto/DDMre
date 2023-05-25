8cce33430f330fb6cf48857fb85069d81c4a7167
Franco Curotto <francocurotto@gmail.com>
2023-05-25 11:05:38 -0400

    make dungeon buttons gui expandable

--- source/Globals.gd
+++ source/Globals.gd
@@ -3,7 +3,7 @@ extends Node
 
 # filepath
 const LIBPATH = "res://LIBRARY.json"
-#const DUNGPATH = "res://dungeons/default.json"
+const DUNGPATH = "res://dungeons/default.json"
 #const DUNGPATH = "res://dungeons/test_pass.json"
 #const DUNGPATH = "res://dungeons/test_target.json"
 #const DUNGPATH = "res://dungeons/test_power.json"
@@ -15,7 +15,7 @@ const LIBPATH = "res://LIBRARY.json"
 #const DUNGPATH = "res://dungeons/test_stops.json"
 #const DUNGPATH = "res://dungeons/test_protect_type.json"
 #const DUNGPATH = "res://dungeons/test_turn_slow_type.json"
-const DUNGPATH = "res://dungeons/test_vortex.json"
+#const DUNGPATH = "res://dungeons/test_vortex.json"
 #const DUNGPATH = "res://dungeons/test_raise_attack.json"
 #const DUNGPATH = "res://dungeons/test_reduce_damage.json"
 #const DUNGPATH = "res://dungeons/test_reply_abilities.json"
@@ -37,8 +37,8 @@ const DUNGPATH = "res://dungeons/test_vortex.json"
 #const DUNGPATH = "res://dungeons/test_dim_cure.json"
 #const DUNGPATH = "res://dungeons/test_multi_attack.json"
 #const DUNGPATH = "res://dungeons/test_negate_atk_abi.json"
-#const RANDOMPOOL = true
-const RANDOMPOOL = false
+const RANDOMPOOL = true
+#const RANDOMPOOL = false
 #const POOL1PATH = "res://dicepools/test_pass.json";const POOL2PATH = "res://dicepools/test_pass.json"
 #const POOL1PATH = "res://dicepools/test_target.json";const POOL2PATH = "res://dicepools/test_target.json"
 #const POOL1PATH = "res://dicepools/test_power.json";const POOL2PATH = "res://dicepools/test_power.json"
