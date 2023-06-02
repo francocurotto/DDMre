350b0df0bf79334eab0fb27e7b93765b51e72b64
Franco Curotto <francocurotto@gmail.com>
2023-06-01 21:01:32 -0400

    add abililities to summon_info

--- source/interface/player_gui/info_display/summon_info/summon_info.gd
+++ source/interface/player_gui/info_display/summon_info/summon_info.gd
@@ -8,12 +8,14 @@ onready var summon_name = $SummonLine/Name
 onready var attack_info = $SummonLine/MonsterStats/AttackInfo
 onready var defense_info = $SummonLine/MonsterStats/DefenseInfo
 onready var health_info = $SummonLine/MonsterStats/HealthInfo
+onready var abilities = $Abilities
 
 # setget functions
 func set_summon(summon, player):
     tla_info.set_tla(summon.card)
     set_summon_name(summon, player)
     set_summon_stats(summon)
+    set_summon_abilities(summon, player)
     visible = true
 
 func clear():
@@ -26,12 +28,19 @@ func set_attacker_power(attacker, attacked):
 # signals callbacks
 func _on_SummonInfo_resized():
     var size_limit = $UpperName.get_size().y + $SummonLine/Name.get_size().y + get_constant("separation") + 1
-    if get_size().y < size_limit:
-        $UpperName.visible = false
-        $SummonLine/Name. visible = true  
-    else:
+    var size_limit2 = size_limit + $Abilities.get_size().y + get_constant("separation")
+    if get_size().y > size_limit2:
         $UpperName.visible = true
         $SummonLine/Name. visible = false
+        $Abilities.visible = true
+    elif get_size().y > size_limit:
+        $UpperName.visible = true
+        $SummonLine/Name. visible = false
+        $Abilities.visible = false  
+    else:
+        $UpperName.visible = false
+        $SummonLine/Name. visible = true
+        $Abilities.visible = false
 
 # private functions
 func set_summon_name(summon, player):
@@ -47,6 +56,15 @@ func set_summon_stats(summon):
         set_monster_stats(summon)
     else: # item info
         set_item_stats()
+
+func set_summon_abilities(summon, player):
+    $Abilities/Ability1.visible = false
+    $Abilities/Ability2.visible = false
+    if summon.is_monster() or player == summon.player:
+        for i in range(len(summon.card.abilities)):
+            var ability = summon.card.abilities[i]
+            var ability_name = abilities.get_child(i)
+            ability_name.text = ability.name
 
 func set_monster_stats(summon):
     attack_info.set_stat_value_color(summon.attack, summon.card.attack)
