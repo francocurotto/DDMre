f2d26dba5c471a77f5acefc7af5d8d2f501c1727
Franco Curotto <francocurotto@gmail.com>
2023-07-05 14:39:48 -0400

    one bug out in path queue

--- source/engine/dungeon/path_queue/move_path_queue.gd
+++ source/engine/dungeon/path_queue/move_path_queue.gd
@@ -1,12 +1,13 @@
 extends "path_queue.gd"
 
-func _init(_dungeon, _monster).(_dungeon, monster):
+func _init(_dungeon, _monster).(_dungeon, _monster):
     pass
 
 # is functions 
 func is_path_extendable(path):
     var max_length = dungeon.get_max_move_tiles(monster)
-    return len(path) < max_length and path[-1].is_passable()
+    var is_passable = path[-1].is_passable(monster) or path[-1]==monster.tile
+    return len(path) < max_length and is_passable
 
 func is_extend_tile(tile):
     return tile.is_reachable() or tile.is_passable()
