60e0de6245cc8d49424d03b2248c6a19ffc937e7
Franco Curotto <francocurotto@gmail.com>
2023-08-17 21:16:00 -0400

    progress dicepool_gui

--- source/interface/player_gui/dicepool_window/dicepool_gui/dicepool_gui.gd
+++ source/interface/player_gui/dicepool_window/dicepool_gui/dicepool_gui.gd
@@ -5,12 +5,9 @@ var selected_dice_gui
 
 # onready variables
 @onready var grid =  $"5-3Ratio/Grid"
-@onready var dice_gui1 = $"5-3Ratio/Grid/DiceGUI1"
 @onready var selector = $"5-3Ratio/Selector"
 
 func _ready():
-    # set selector size
-    selector.scale = dice_gui1.size / selector.get_rect().size
     # connections
     for dice_gui in grid.get_children():
         dice_gui.dice_entered.connect(on_dice_entered)
@@ -19,5 +16,6 @@ func _ready():
 func on_dice_entered(dice_gui):
     if dice_gui != selected_dice_gui:
         selected_dice_gui = dice_gui
-        selector.position = selected_dice_gui.position
+        selector.scale = selected_dice_gui.size / selector.get_rect().size
+        selector.position = grid.position + selected_dice_gui.position
         selector.visible = true
