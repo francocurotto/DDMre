1abc6906cb94191be9c8f62adf6d95c11e2dbae4
Franco Curotto <francocurotto@gmail.com>
2023-08-24 08:32:34 -0400

    improvement in attribute info

--- source/interface/player_gui/common/attributes_info/attribute_info/attribute_info.gd
+++ source/interface/player_gui/common/attributes_info/attribute_info/attribute_info.gd
@@ -2,5 +2,5 @@
 extends PanelContainer
 
 func _on_resized():
-    var icon_size = $HBox/AspectRatio/AttributeIcon.size
+    var icon_size = $HBox/AttributeIcon.size
     $HBox/AttributeValue.add_theme_font_size_override("font_size", icon_size.y*0.5)
