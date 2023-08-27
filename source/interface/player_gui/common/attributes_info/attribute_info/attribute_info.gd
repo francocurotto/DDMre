63d456e99f13bd979ab820f854a55a6bddd462fc
Franco Curotto <francocurotto@gmail.com>
2023-08-27 12:37:15 -0400

    progress in attributes info

--- source/interface/player_gui/common/attributes_info/attribute_info/attribute_info.gd
+++ source/interface/player_gui/common/attributes_info/attribute_info/attribute_info.gd
@@ -1,26 +1,21 @@
 @tool
-extends PanelContainer
+extends MarginContainer
 
 # export variables
 @export_range(0, 60, 10) var value = 30 :
     set(_value):
         value = _value
-        value_label.text = "%2d" % value
+        if has_node("%ValueLabel"):
+            %ValueLabel.text = "%2d" % value
 
 @export_enum("ATTACK", "DEFENSE", "HEALTH") var attribute : String = "ATTACK" :
     set(_attribute):
         attribute = _attribute
-        attribute_icon.texture = load("res://art/icons/ATTR_%s.svg"%attribute)
-
-# variables
-var attribute_icon
-var value_label
-
-func _enter_tree():
-    attribute_icon = $HBox/AttributeIcon
-    value_label = $HBox/ValueLabel
+        if has_node("%AttributeIcon"):
+            var texture = load("res://art/icons/ATTR_%s.svg"%attribute)
+            %AttributeIcon.texture = texture
 
 # signals callbacks
 func _on_resized():
-    var icon_size = attribute_icon.size
-    value_label.add_theme_font_size_override("font_size", icon_size.y-5)
+    var icon_size = %AttributeIcon.size
+    %ValueLabel.add_theme_font_size_override("font_size", icon_size.y-5)
