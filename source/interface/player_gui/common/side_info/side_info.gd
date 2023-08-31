154e00d0814076bf775c69d98ea7e0864704eb47
Franco Curotto <francocurotto@gmail.com>
2023-08-30 22:40:09 -0400

    progress in side info

--- source/interface/player_gui/common/side_info/side_info.gd
+++ source/interface/player_gui/common/side_info/side_info.gd
@@ -9,13 +9,18 @@ var type : String = "DRAGON" :
         if has_node("SideIcon"):
             $SideIcon.self_modulate = COLORS[type]
 
-@export_enum("SUMMON", "MOVEMENT", "ATTACK", "DEFENSE", "MAGIC", "TRAP") 
-var crest : String = "SUMMON" :
+@export_enum("SUMMON2", "MOVEMENT", "ATTACK", "DEFENSE", "MAGIC", "TRAP") 
+var crest : String = "SUMMON2" :
     set(_crest):
         crest = _crest
         if has_node("CrestIcon"):
-            var texture = load("res://art/icons/CREST_%s.svg" % crest)
-            $CrestIcon.texture = texture
+            set_crest_and_mult()
+
+@export_range(1, 9) var mult = 1 :
+    set(_mult):
+        mult = _mult
+        if has_node("%Mult"):
+            set_crest_and_mult()
 
 # constants
 const COLORS = {
@@ -25,3 +30,25 @@ const COLORS = {
     "BEAST"       : Color(0.3,1.0,0.3),
     "WARRIOR"     : Color(0.3,0.3,1.0),
     "ITEM"        : Color(0.3,0.3,0.3)}
+
+# signals callbacks
+func _on_resized():
+    var side_size = $SideIcon.size.y
+    %Mult.add_theme_font_size_override("font_size", side_size/3)
+    %Mult.add_theme_constant_override("outline_size", side_size/8)
+
+# private functions
+func set_crest_and_mult():
+    # set crest icon
+    var texture = load("res://art/icons/CREST_%s.svg" % crest)
+    $CrestIcon.texture = texture
+    %Mult.text = str(mult)
+    # set mult
+    if crest == "SUMMON2":
+        %Mult.size_flags_horizontal = SIZE_SHRINK_CENTER
+        %Mult.size_flags_vertical   = SIZE_SHRINK_CENTER
+        %Mult.visible = true
+    else:
+        %Mult.size_flags_horizontal = SIZE_SHRINK_END
+        %Mult.size_flags_vertical   = SIZE_SHRINK_END
+        %Mult.visible = mult != 1
