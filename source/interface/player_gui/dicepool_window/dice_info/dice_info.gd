db9ebe86ed4f6a1a5dec4e25dfa72b9645721c09
Franco Curotto <francocurotto@gmail.com>
2023-09-07 22:20:05 -0300

    fix dice info font sizes

--- source/interface/player_gui/dicepool_window/dice_info/dice_info.gd
+++ source/interface/player_gui/dicepool_window/dice_info/dice_info.gd
@@ -6,9 +6,9 @@ func _ready():
 # public functions
 func setup(dice):
     var texture = load("res://art/icons/SUMMON_%s.svg" % dice.card.type)
+    $HBox.visible = true
     %SummonIcon.texture = texture
     %Name.text = dice.card.name
     %AttributesInfo.setup(dice.card)
     %SidesInfo.setup(dice)
-    %AbilitiesInfo.setup(dice.card.abilities)
-    $HBox.visible = true
+    %AbilitiesInfo.setup(dice.card.abilities)    
