a5d37c0446559a88cfd2bacd82c0c45d8be8d6a4
Franco Curotto <francocurotto@gmail.com>
2024-02-21 00:29:29 -0300

    change dicepool button

--- source/interface/player_gui/player_info/player_info.gd
+++ source/interface/player_gui/player_info/player_info.gd
@@ -5,7 +5,7 @@ var player
 
 # public functions
 func setup(_player):
-	player = _player
-	%HeartsInfo.setup(player)
-	%CrestpoolInfo.setup(player.crestpool)
-	
+    player = _player
+    %HeartsInfo.setup(player)
+    %CrestpoolInfo.setup(player.crestpool)
+    
