fe06e63c87ac3dd130c5a62792f83b8afabc8c98
Franco Curotto <francocurotto@gmail.com>
2024-02-22 20:00:43 -0300

    add common abilities icons

--- source/interface/player_gui/summon_info/summon_info.gd
+++ source/interface/player_gui/summon_info/summon_info.gd
@@ -1,5 +1,8 @@
 extends PanelContainer
 
+# constants
+const COMMON_ABILITIES = ["ARCHER", "FLY", "NEUTRAL", "TUNNEL"]
+
 # signals callbacks
 func on_tile_gui_toggled(tile_gui, toggled_on):
     var content = tile_gui.tile.content
@@ -12,10 +15,10 @@ func on_tile_gui_toggled(tile_gui, toggled_on):
         %TypeIcon.texture = load("res://assets/icons/SUMMON_%s.svg" % content.card.type)
         # set abilities
         if len(content.card.abilities) == 2:
-            %AbilityIcon1.texture = load("res://assets/icons/ABILITY.svg")
-            %AbilityIcon2.texture = load("res://assets/icons/ABILITY.svg")
+            %AbilityIcon1.texture = get_ability_icon(content.card.abilities[0])
+            %AbilityIcon2.texture = get_ability_icon(content.card.abilities[1])
         elif len(content.card.abilities) == 1:
-            %AbilityIcon1.texture = load("res://assets/icons/ABILITY.svg")
+            %AbilityIcon1.texture = get_ability_icon(content.card.abilities[0])
             %AbilityIcon2.texture = null
         else:
             %AbilityIcon1.texture = null
@@ -38,3 +41,10 @@ func on_tile_gui_toggled(tile_gui, toggled_on):
         %AbilityIcon1.texture = null
         %AbilityIcon2.texture = null
         %BattleAttributes.visible = false
+
+# private  functions
+func get_ability_icon(ability):
+    if ability.name in COMMON_ABILITIES:
+        return load("res://assets/icons/ABILITY_%s.svg" % ability.name)
+    else:
+        return load("res://assets/icons/ABILITY.svg")
