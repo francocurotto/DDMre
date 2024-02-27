46e66e3fc35359fc87870e15fe105a54e2790572
Franco Curotto <francocurotto@gmail.com>
2024-02-27 15:06:52 -0300

    delete dicepool_window

--- source/interface/player_gui/dicepool_window/dicepool_window.gd
+++
@@ -1,9 +0,0 @@
-extends MarginContainer
-
-# public functions
-func setup(dicepool):
-    %DicepoolGUI.setup(dicepool)
-
-# signals callbacks
-func _on_dicepool_gui_dice_gui_selected(dice):
-    %DiceInfo.setup(dice)
