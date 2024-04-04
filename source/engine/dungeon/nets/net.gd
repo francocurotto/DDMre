7beb7c8085bced8df2a04d2556956df76cdb416c
Franco Curotto <francocurotto@gmail.com>
2024-04-04 19:45:35 -0300

    progress in net refactor

--- source/engine/dungeon/nets/net.gd
+++ source/engine/dungeon/nets/net.gd
@@ -1,64 +1,71 @@
 extends RefCounted
+## A collection of contiguous positions that represents the unfolding of a dice 
+## during dimension.
+##
+## A net is used to determine the positions of path tiles during dimension. It
+## can be moved, rotated, fliped, and it tracks it center position, where the
+## summon of the dimension will be located.
 
-# variables
-var poslist
-var centeridx
-var centerpos : get = get_centerpos
-var unfolds : 
-    get:
-        var _unfolds = []
-        for i in range(1, len(poslist)):
-            var pos = poslist[i]
-            for prev_pos in poslist.slice(0, i):
-                var direction = pos - prev_pos
-                if direction.length() <= 1:
-                    _unfolds.append(direction)
+#region variables
+var positions ## net positions
+var center : ## net center position
+     get: return positions[0]
+var unfolds : get = get_unfolds ## unfold directions
+#endregion
+
+#region public function
+## Add an [param offset] to all positions in net.
+func add_offset(offset):
+    positions = positions.map(func(pos): pos + offset)
+
+## Apply all transformations in array [param transformations] to net.
+func apply_trans_list(transformations):
+    for trans in transformations:
+        apply_trans(trans)
+#endregion
+
+#region private functions
+## Get the directions (vectors) a dice unfolds to create the net. Each
+## direction represents the edge a tile "appears" from a previously set tile.
+func get_unfolds():
+    var _unfolds = []
+    for i in range(1, len(positions)):
+        var pos = positions[i]
+        for prev_pos in positions.slice(0, i):
+            var direction = pos - prev_pos
+            if direction.length() <= 1:
+                _unfolds.append(direction)
+                break
         return _unfolds
 
-func _init():
-    create_poslist()
-    centeridx = poslist.find(Vector2i(0,0))
-
-# setget functions
-func get_centerpos():
-    return poslist[centeridx]
-
-# public function
-func create_poslist():
-    pass
-
-func add_offset(offset):
-    for i in len(poslist):
-        poslist[i] = poslist[i]+offset
-
-func apply_trans_list(trans_list):
-    for trans in trans_list:
-        apply_trans(trans)
-
-# private functions
+## Apply transformation [param trans] to the net. It can be one of the
+## following transformations:
+## - TCW: turn clockwise
+## - TAW: turn anti-clockwise
+## - FUD: flip up-down
+## - FLR: flip left-right
 func apply_trans(trans):
     match trans:
-        "TCW" : apply_turn_clock_wise()
-        "TAW" : apply_turn_anticlock_wise()
-        "FUD" : apply_flip_up_down()
-        "FLR" : apply_flip_left_right()
+        "TCW" : positions = positions.map(apply_turn_clockwise)
+        "TAW" : positions = positions.map(apply_turn_anti_clockwise)
+        "FUD" : positions = positions.map(apply_flip_up_down)
+        "FLR" : positions = positions.map(apply_flip_left_right)
 
-func apply_turn_clock_wise():
-    for i in len(poslist):
-        var pos = poslist[i]
-        poslist[i].x = self.centerpos.x+(pos.y-self.centerpos.y) # mathemagics
-        poslist[i].y = self.centerpos.y-(pos.x-self.centerpos.x) # mathemagics
+func apply_turn_clockwise(pos):
+    var x = center.x + (pos.y - center.y) # mathemagics
+    var y = center.y - (pos.x - center.x) # mathemagics
+    return Vector2i(x, y)
 
-func apply_turn_anticlock_wise():
-    for i in len(poslist):
-        var pos = poslist[i]
-        poslist[i].x = self.centerpos.x-(pos.y-self.centerpos.y) # mathemagics
-        poslist[i].y = self.centerpos.y+(pos.x-self.centerpos.x) # mathemagics
+func apply_turn_anti_clockwise(pos):
+    var x = center.x - (pos.y - center.y) # mathemagics
+    var y = center.y + (pos.x - center.x) # mathemagics
+    return Vector2i(x, y)
 
-func apply_flip_up_down():
-    for i in len(poslist):
-        poslist[i].y = 2*self.centerpos.y-poslist[i].y # mathemagics
+func apply_flip_up_down(pos):
+    var y = 2*center.y - pos.y # mathemagics
+    return Vector2i(pos.x, y)
 
-func apply_flip_left_right():
-    for i in len(poslist):
-        poslist[i].x = 2*self.centerpos.x-poslist[i].x # mathemagics
+func apply_flip_left_right(pos):
+    var x = 2*center.x - pos.x # mathemagics
+    return Vector2i(x, pos.y)
+#endregion
