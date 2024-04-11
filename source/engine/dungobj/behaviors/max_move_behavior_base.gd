dd3b3d6d0b9128557be98d3ef346171fa8e0dac7
Franco Curotto <francocurotto@gmail.com>
2024-04-11 18:28:26 -0400

    .

--- source/engine/dungobj/behaviors/max_move_behavior_base.gd
+++ source/engine/dungobj/behaviors/max_move_behavior_base.gd
@@ -1,37 +1,38 @@
 extends RefCounted
+## Monster behavior to handle the max number of tiles it can move per turn.
+##
+## Max move behavior base tracks the number of tiles a monster have moved each
+## turn and limits its movement to the lowest value in an array of turn move
+## limits. Normally, a monster has no limits of movement so the default value
+## on [code]turn_move_limits[/code] is infinity. Some monsters have abilities
+## that limits its movement, so limit values are added to 
+## [code]turn_move_limits[/code] array. Movement can be suppresed entirely by
+## adding 0 to [code]turn_move_limits[/code].
 
-# variables
-var turn_move_limits = [INF]
-var turn_move_count = 0
+#region variables
+var turn_move_limits = [INF] ## Array of all movement limits for monster
+var turn_move_count = 0 ## Variable to track the number of moves in turn
+##endregion
 
-# public functions
+#region public functions
+## Get the current maximum movement allowed by a monster, taking into account
+## turn move limits added by abilities.
 func get_max_move():
-    """
-    Get the maximum movement allowed by a monster, taking into account turn
-    move limits added by abilities.
-    """
     return turn_move_limits.min() - turn_move_count
 
+## Add [param move] value to current turn move count.
 func update_turn_move_count(move):
-    """
-    Update turn move count with new move steps.
-    """
     turn_move_count += move
 
+## Reset turn move count to 0. Usually done at the end of a turn.
 func reset_turn_move_count():
-    """
-    Return turn move count to 0 (e.g. after the start of a new turn).
-    """
     turn_move_count = 0 
 
+## Add value [param limit] to the turn move limits array.
 func add_limit(limit):
-    """
-    Add a limit to the turn move limits array.
-    """
     turn_move_limits.append(limit)
 
+## Remove value [param limit] from the turn move limits array.
 func remove_limit(limit):
-    """
-    Remove a limit from the turn move limits array.
-    """
     turn_move_limits.erase(limit)
+#endregion
