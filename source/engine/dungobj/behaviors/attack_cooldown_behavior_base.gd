18f240da834d4b3239367dcb2d01b86da4c2551c
Franco Curotto <francocurotto@gmail.com>
2024-04-11 08:48:05 -0400

    progress in behavior refactor

--- source/engine/dungobj/behaviors/attack_cooldown_behavior_base.gd
+++ source/engine/dungobj/behaviors/attack_cooldown_behavior_base.gd
@@ -1,25 +1,31 @@
 extends RefCounted
+## Monster behavior on how to handle the attack cooldown.
+##
+## The attack cooldown behavior base has a max attack value and a counter to
+## track the number of attacks a monster can perform per turn. Normally,
+## monsters can only attack once per turn, hence [code]max_attack=1[/code].
+## Some monster have abilities that let them attack more than once, in that 
+## case [code]max_attack[/code] is set to a higher value. Also attacks can be
+## disable entirely by setting [code]max_attack=0[/code].
 
-# variables
-var max_attacks = 1
-var counter = 0
+#region variables
+var max_attacks = 1 ## Maximum number of attacks a monster can perform per turn
+var counter = 0 ## Variable to track the current number of attacks in turn
+#endregion
 
-# public functions
-func can_attack():
-    """
-    Check if monster can attack given the attack cooldown.
-    """
-    return counter < max_attacks
-
+#region public functions
+## Increase cooldown counter, usually after an attack.
 func activate():
-    """
-    Increase cooldown counter, usually after an attack.
-    """
     counter += 1
 
+## Reset cooldown counter, usually after turn ends.
 func reset():
-    """
-    Reset cooldown, usually after turn ends.
-    """
     counter = 0
+#endregion
+
+#region is functions
+## Check if monster can attack given the attack cooldown.
+func can_attack():
+    return counter < max_attacks
+##endregion
 
--- source/engine/dungobj/behaviors/damage_behavior_base.gd
+++ source/engine/dungobj/behaviors/damage_behavior_base.gd
@@ -1,37 +1,48 @@
 extends RefCounted
+## Monster behavior to handle how damage is dealt to the monster.
+##
+## Damage behavior base compute the reduction of health points of a monster
+## given an attack with a specific damage value. Normally, damage is
+## substracted directly to health points. However, monster abilities can add
+## reductions and limits to the damage dealt. Damage can be suppresed entirely
+## by setting the damage limit to 0. Additionally, the damage 
+## [code]receiver[/code] can be change by an ability, to redirect damage to
+## other monster.
 
-# variables
-var damage_limits = [INF]
-var ability_reduce = 0
-var receiver
+#region variables
+var damage_limits = [INF] ## Array of damage limits. By default, no limits
+var ability_reduce = 0 ## Damage reduction added by ability
+var receiver ## monster receiver of the damage
+#endregion
 
+#region builtin functions
 func _init(monster):
     receiver = monster
+#endregion
 
-# public functions
+#region public functions
+## Compute damage taking in account limits and reductions, and dealt damage to receiver. If receiver health pointa reaches 0, destry monster.
 func receive_damage(damage):
     receiver.health -= get_inflicted_damage(damage)
     if receiver.health <= 0:
         receiver.destroy()
 
+## Add a [param limit] to the damage limits array. During computation, the
+## lowest limit is considered.
 func add_limit(limit):
-    """
-    Add a limit to the damage limits array.
-    """
     damage_limits.append(limit)
 
+## Remove a [param limit] from the damage limits array.
 func remove_limit(limit):
-    """
-    Remove a limit from the damage limits array.
-    """
     damage_limits.erase(limit)
+#endregion
 
-# private functions
+#region private functions
+## Get the inflicted damage on a monster considering the damage of an attack,
+## but also the damage limits and reductions that abilities may have imposed.
+## When multiple limits are.in place, use the lowest limit.
 func get_inflicted_damage(damage):
-    """
-    Get the inflicted damage on a monster considering the damage of an attack,
-    but also the damage limit that abilities may have imposed.
-    """
     var reduced_damage = max(0, damage-ability_reduce)
     var limited_damage = min(reduced_damage, damage_limits.min())
     return limited_damage
+#endregion
