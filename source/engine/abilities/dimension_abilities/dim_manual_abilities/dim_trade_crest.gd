f333025b2a2240d4ae7c65413d7269062bbb67ba
Franco Curotto <francocurotto@gmail.com>
2024-04-24 19:12:00 -0400

    progress ability refactor

--- source/engine/abilities/dimension_abilities/dim_manual_abilities/dim_trade_crest.gd
+++ source/engine/abilities/dimension_abilities/dim_manual_abilities/dim_trade_crest.gd
@@ -1,14 +1,24 @@
 extends "dim_manual_ability.gd"
+## DIMTRADECREST ability.
+##
+## Pay a certain number of crests of any type to gain a different amount of 
+## other type.
 
-var cost
-var amount
+#region variables
+var cost   ## Number of crests to pay in trade
+var amount ## Number of crests to gain in trade
+#endregion
 
+#region builtin functions
 func _init(ability_dict):
     super(ability_dict)
     cost = ability_dict["COST"]
     amount = ability_dict["AMOUNT"]
+#endregion
 
-# public functions
+#region public functions
+## Activate ability using parameters [param activate_dict]. Trade crests.
 func activate(activate_dict):
     pay_crests(activate_dict["pay_crest"], cost)
     gain_crests(activate_dict["gain_crest"], amount)
+#endregion
