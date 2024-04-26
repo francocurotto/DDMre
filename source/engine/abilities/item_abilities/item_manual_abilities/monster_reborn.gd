e76e7dd8aa9d40f935a688df90183d9cf681c9e4
Franco Curotto <francocurotto@gmail.com>
2024-04-26 17:24:26 -0400

    progress in abilities refactor

--- source/engine/abilities/item_abilities/item_manual_abilities/monster_reborn.gd
+++ source/engine/abilities/item_abilities/item_manual_abilities/monster_reborn.gd
@@ -1,27 +1,35 @@
 extends "item_manual_ability.gd"
+## MONSTERREBORN ability.
+##
+## Make a dimensioned dice, where the summoned monster has already been
+## destroyed, available again for dimension.
 
-func _init(ability_dict):
-    super(ability_dict)
-    pass
-
-# public functions
+#region public functions
+## Activate ability. Use [param activate_dict] parameter to get the dice to
+## mark as not dimensioned, and use [param monster] to erase the selected 
+## monster from the graveyard array.
 func activate(monster, activate_dict):
     var dice = monster.player.dicepool[activate_dict["diceidx"]]
     dice.dimensioned = false
     var reborn_monster = get_reborn_monster(monster.player, dice)
     monster.player.graveyard.erase(reborn_monster)
 
+## Get an array of dice available to revive. Use [param monster] to get the 
+## array of monsters from player in graveyard. Used for the ability interface
+## that the player use to activate the ability.
 func get_select_dice(monster):
-    var dicelist = []
-    for dead_monster in monster.player.graveyard:
-        for dice in monster.player.dicepool:
-            if dice.card == dead_monster.card:
-                dicelist.append(dice)
-    return dicelist
+    var graveyard = monster.player.graveyard
+    var dicepool = monster.player.dicepool
+    var cards = graveyard.map(func(_monster): return _monster.card)
+    var dice_array = dicepool.filter(func(dice): return dice.card in cards)
+    return dice_array
+#endregion
 
-# private functions
+#region private functions
+## Get the monster in [param player] graveyard that was summoned with
+## [param dice].
 func get_reborn_monster(player, dice):
-    #GODOT4: use array filter
     for monster in player.graveyard:
         if monster.card == dice.card:
             return monster
+#endregion
