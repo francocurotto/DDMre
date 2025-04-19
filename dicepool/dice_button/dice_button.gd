c73bd172af0b0bba757e33f4c76fdae4075200f4
Franco Curotto <francocurotto@gmail.com>
2025-04-19 15:00:17 -0400

    progress in dicepool

--- dicepool/dice_button/dice_button.gd
+++ dicepool/dice_button/dice_button.gd
@@ -1,11 +1,23 @@
 extends AspectRatioContainer
 
+#region signals
+signal button_toggled
+#endregion
+
+#region public variables
+var disabled : bool = false :
+	set(_disabled):
+		disabled = _disabled
+		%Button.disabled = disabled
+#endregion
+
 #region public functions
 func set_dice(dice_dict):
-	$MarginContainer/SubViewportContainer/SubViewport/Dice.set_dice(dice_dict)
+	%Dice.set_dice(dice_dict)
 #endregion
 
 #region signals callbacks
-func _on_button_pressed() -> void:
-	print("Dice Button pressed")
+func _on_button_toggled(toggled_on: bool) -> void:
+	button_toggled.emit(toggled_on, self)
+	print("Button toggled (%s)" % toggled_on)
 #endregion
--- dicepool/dicepool.gd
+++ dicepool/dicepool.gd
@@ -1,6 +1,38 @@
 extends PanelContainer
 
+#region public variables
+var buttons = []
+var roll_dice_buttons = []
+#endregion
+
+#region builtin functions
+func _ready() -> void:
+	for button in $Grid.get_children():
+		buttons.append(button)
+		button.button_toggled.connect(on_dice_button_toggled)
+#endregion
+
 #region public functions
 func set_dice(i, dice_dict):
 	$Grid.get_child(i).set_dice(dice_dict)
+#endregion
+
+#region signals callbacks
+func on_dice_button_toggled(toggled_on, button):
+	if toggled_on:
+		on_dice_button_pressed(button)
+	else:
+		on_dice_button_released(button)
+
+func on_dice_button_pressed(pressed_button):
+	roll_dice_buttons.append(pressed_button)
+	if len(roll_dice_buttons) >= 3:
+		for button in buttons:
+			if button not in roll_dice_buttons:
+				button.disabled = true
+
+func on_dice_button_released(released_button):
+	roll_dice_buttons.erase(released_button)
+	for button in buttons:
+		button.disabled = false
 #endregion
