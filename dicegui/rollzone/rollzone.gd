4f9352649a3a90fbf1562a5bcdb77635b776720a
Franco Curotto <francocurotto@gmail.com>
2025-04-22 09:06:02 -0400

    progress in roll

--- dicegui/rollzone/rollzone.gd
+++ dicegui/rollzone/rollzone.gd
@@ -6,27 +6,35 @@ var DICEPOS = [Vector3(-2.5,0,0), Vector3(0,0,0), Vector3(2.5,0,0)]
 
 #region private variables
 var dragging = false
-var throw_velocity = Vector2.ZERO
+var roll_velocity = Vector2.ZERO
 #endregion
 
 #region builtin functions
 func _input(event):
 	var rollzone_selected = get_parent().current_tab == 2
-	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		# Check if the touch is inside the drag area
-		if get_global_rect().has_point(event.position) and rollzone_selected:
-			if event is InputEventScreenTouch and event.pressed:
-				dragging = true
-			elif event is InputEventScreenDrag and dragging:
-				throw_velocity = event.velocity
-				print(str(throw_velocity))
-			elif event is InputEventScreenTouch and not event.pressed and dragging:
-				print("THROW!: " + str(throw_velocity))
-				dragging = false
-				throw_velocity = Vector2.ZERO
-		else:
-			dragging = false
-			throw_velocity = Vector2.ZERO
+	# check if roll completed
+	if len(%DiceList.get_children()) >= 3:
+		# check for touch or drag
+			if event is InputEventScreenTouch or event is InputEventScreenDrag:
+				# Check if the touch is inside the drag area
+				if get_global_rect().has_point(event.position) and rollzone_selected:
+					# if touch, dragging started
+					if event is InputEventScreenTouch and event.pressed:
+						dragging = true
+					# if drag, register velocity
+					elif event is InputEventScreenDrag and dragging:
+						roll_velocity = event.velocity
+					# if drag released, roll dice
+					elif event is InputEventScreenTouch and not event.pressed and dragging:
+						for dice in %DiceList.get_children():
+							dice.gravity_scale = 1
+						print("ROLL!: " + str(roll_velocity))
+						dragging = false
+						roll_velocity = Vector2.ZERO
+				# if outside the drag area, reset dragging
+				else:
+					dragging = false
+					roll_velocity = Vector2.ZERO
 
 #region public functions
 func update_dice(dice_buttons):
