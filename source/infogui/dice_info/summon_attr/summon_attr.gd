84b6185ab37e7a917d481fe46f70fa3d0eeeac3b
Franco Curotto <francocurotto@gmail.com>
2025-05-06 08:56:05 -0400

    start dice info

---
+++ source/infogui/dice_info/summon_attr/summon_attr.gd
@@ -0,0 +1,31 @@
+@tool
+extends HBoxContainer
+
+#region export variables
+@export_enum("LEVEL", "ATTACK", "DEFENSE")
+var attr : String = "LEVEL" :
+	set(_attr):
+		attr = _attr
+		$Attr.texture = load("res://assets/ATTR_%s.svg" % attr)
+
+@export var value : int = 1 :
+	set(_value):
+		value = _value
+		$Value.text = str(value)
+		set_value_color()
+
+@export var original_value : int = 1 :
+	set(_original_value):
+		original_value = _original_value
+		set_value_color()
+#endregion
+
+#region private functions
+func set_value_color():
+	if value == original_value:
+		$Value.modulate = Color(1,1,1)
+	elif value > original_value:
+		$Value.modulate = Color(1,0,0)
+	else: # value < original value
+		$Value.modulate = Color(0,0,1)
+#endregion
