8e4069fd88950d5c4ed0f06bd017172afa376532
Franco Curotto <francocurotto@gmail.com>
2025-05-08 20:34:37 -0400

    progress in summon info

--- source/infogui/summon_info/summon_info.gd
+++ source/infogui/summon_info/summon_info.gd
@@ -8,9 +8,16 @@ func _ready() -> void:
 #region signals callbacks
 func on_dice_focus_entered(dice):
 	%SidesRow.set_sides(dice.sides, dice.summon.type)
+	%SummonIcon.texture = load("res://assets/SUMMON_%s.svg" % dice.summon.type)
 	%Name.text = dice.summon.summon_name
 	%Level.original_value = dice.summon.level
 	%Attack.original_value = dice.summon.attack
 	%Defense.original_value = dice.summon.defense
 	%Hearts.original_health = dice.summon.health
+	# clear attack,defense and hearts if item
+	var is_item = dice.summon.type == "ITEM"
+	%Attack.visible = not is_item
+	%Defense.visible = not is_item
+	%Hearts.visible = not is_item
+	visible = true
 #endregion
