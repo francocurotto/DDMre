7a71aa8984b96d118564c29bb1083084ecce6732
Franco Curotto <francocurotto@gmail.com>
2025-06-03 09:22:48 -0400

    add docs

---
+++ docs/CODE_STYLE.md
@@ -0,0 +1,35 @@
+# Code Lineup
+
+01. @tool
+02. class_name
+03. extends
+04. docstring
+
+05. signals
+06. enums
+07. constants
+08. preloads
+09. @export variables
+10. public variables
+11. private variables
+12. @onready variables
+
+12. optional built-in virtual `_init()` method
+13. optional built-in virtual `_enter_tree()` method
+14. built-in virtual `_ready()` method
+15. remaining built-in virtual methods
+16. static methods
+17. public methods
+18. signals callbacks
+20. private methods
+21. subclasses
+
+# Naming Convension
+- folders: snake_case
+- scenes files: snake_case
+- script files: snake_case
+- nodes: PascalCase
+- functions: snake_case
+- variables: snake_case
+- enums: CONSTANT_CASE
+- constants: CONSTANT_CASE
--- source/scenes/dungeon/dungeon.gd
+++ source/scenes/dungeon/dungeon.gd
@@ -6,11 +6,29 @@ const DIMDICE_POS = Vector3(0, 1, -3)
 
 #region private variables
 var dimdice = null
+var tiles = []
 #endregion
 
 #region builtin functions
 func _ready() -> void:
 	Events.dimdice_selected.connect(on_dimdice_selected)
+	for row in $Rows.get_children():
+		for tile in row.get_children():
+			tiles.append(tile)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch and event.pressed:
+		var touch_pos = get_viewport().get_mouse_position()
+		var camera = get_viewport().get_camera_3d()
+		var ray_origin = camera.project_ray_origin(touch_pos)
+		var ray_target = ray_origin + camera.project_ray_normal(touch_pos) * 1000
+		var space_state = get_world_3d().direct_space_state
+		var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target)
+		var result = space_state.intersect_ray(query)
+		if result:
+			var selected_object = result["collider"]
+			if selected_object in tiles and dimdice:
+				dimdice.position = selected_object.global_position + Vector3(0,1,0)
 #endregion
 
 #region signals callbacks
