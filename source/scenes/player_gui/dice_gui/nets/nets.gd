3dfdfade53747d9ffaf52bc796f4d1f0c7382863
Franco Curotto <francocurotto@gmail.com>
2025-06-16 21:28:53 -0400

    progress in net selection

--- source/scenes/player_gui/dice_gui/nets/nets.gd
+++ source/scenes/player_gui/dice_gui/nets/nets.gd
@@ -9,4 +9,10 @@ func _ready() -> void:
 	for button in %GridContainer.get_children():
 		button.button_group = button_group
 	Net.type = button_group.get_pressed_button().type
+	button_group.pressed.connect(on_button_pressed)
+#endregion
+
+#region signals callbacks
+func on_button_pressed(button):
+	Net.type = button.type
 #endregion
