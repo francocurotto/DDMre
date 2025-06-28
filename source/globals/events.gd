27f92f41934d174ac18d254abb76974eff064c15
Franco Curotto <francocurotto@gmail.com>
2025-06-27 22:21:05 -0400

    fix comment

--- source/globals/events.gd
+++ source/globals/events.gd
@@ -1,7 +1,7 @@
 extends Node
 
 signal roll_started ## Emitted when player starts a roll
-signal crest_side_rolled ## Emitted when player starts a roll
+signal crest_side_rolled ## Emitted dice roll a crest side
 signal dice_button_focus_entered ## Emitted when player focus dice button in dicepool
 signal dicegui_tab_changed ## Emitted when player changes tab in dicegui
 signal dimdice_selected ## Emitted when player selects a dice to dimension
