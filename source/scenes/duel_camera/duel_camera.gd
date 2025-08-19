eda92a2ddc7f4662afcdb5b798dbb03876725131
Franco Curotto <francocurotto@gmail.com>
2025-08-18 20:55:02 -0400

    debugging camera

--- source/scenes/duel_camera/duel_camera.gd
+++ source/scenes/duel_camera/duel_camera.gd
@@ -5,6 +5,9 @@ const PAN_SPEED = 0.02
 const ZOOM_SPEED = 0.02
 #endregion
 
+func _ready() -> void:
+	print(transform.basis.z)
+
 #region public functions
 func pan(velocity):
 	var delta = get_process_delta_time()
@@ -14,5 +17,6 @@ func pan(velocity):
 
 func zoom(zoom_force):
 	var delta = get_process_delta_time()
+	print(transform.basis.z * ZOOM_SPEED * zoom_force * delta)
 	translate(transform.basis.z * ZOOM_SPEED * zoom_force * delta)
 #endregion
