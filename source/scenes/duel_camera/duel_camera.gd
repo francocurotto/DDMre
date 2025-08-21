a3d9afa0428e8d9e8e2f9aaec1126a0d7b9d2a4f
Franco Curotto <francocurotto@gmail.com>
2025-08-21 07:56:47 -0400

    adjust camera prop

--- source/scenes/duel_camera/duel_camera.gd
+++ source/scenes/duel_camera/duel_camera.gd
@@ -2,7 +2,7 @@ extends Camera3D
 
 #region constants
 const PAN_SPEED = 0.05
-const ZOOM_SPEED = 40
+const ZOOM_SPEED = 400
 #endregion
 
 #region public functions
@@ -11,11 +11,13 @@ func pan(velocity):
 	var movement = Vector3(velocity.x, 0, velocity.y)
 	var delta_position = -1 * PAN_SPEED * movement * delta
 	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self, "position", position + delta_position, 0.1)
 
 func zoom(factor):
 	var delta = get_process_delta_time()
 	var delta_position = transform.basis.z * ZOOM_SPEED * factor * delta
 	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self, "position", position + delta_position, 0.1)
 #endregion
